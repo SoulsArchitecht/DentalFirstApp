741c0ebc10dcb270b481671b40abbb995662529d
Serge <sshibkojavadev@gmail.com>
2025-05-14 21:06:43 +0800

    init commit

---
+++ app/src/main/java/ru/sshibko/dentalfirstapp/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.sshibko.dentalfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
