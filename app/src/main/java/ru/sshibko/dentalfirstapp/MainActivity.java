64db5416b2d1ca224cf85220d220dfb62f00be66
Serge <sshibkojavadev@gmail.com>
2025-05-15 21:34:04 +0800

    added: basic activities, settings and styles

--- app/src/main/java/ru/sshibko/dentalfirstapp/MainActivity.java
+++ app/src/main/java/ru/sshibko/dentalfirstapp/MainActivity.java
@@ -1,7 +1,11 @@
 package ru.sshibko.dentalfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button searchButton = findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        });
     }
 }
